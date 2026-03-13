6bd1a1cced94db4650b3a129d780df0ddd5217d1
Grancanash <grancanash@gmail.com>
2026-03-13 12:18:51 +0000

    Se cambia url para la api (Ahora va por cloudflare y ssl)

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,6 @@
 export const CONFIG = {
 //   API_URL: 'http://localhost:8000/api',
-  API_URL: 'http://217.154.178.102/api',
+//   API_URL: 'http://217.154.178.102/api',
+  API_URL: 'https://generate-prizes-father-belong.trycloudflare.com/api',
   APP_NAME: 'Mi Cine'
 } as const;
