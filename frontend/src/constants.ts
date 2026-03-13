6e18673a99f12c189e03625344d8dd8f19a40e1c
Grancanash <grancanash@gmail.com>
2026-03-13 13:46:40 +0000

    Actualización url cloudflare

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
 //   API_URL: 'http://localhost:8000/api',
 //   API_URL: 'http://217.154.178.102/api',
-  API_URL: 'https://oven-relying-addressed-puerto.trycloudflare.com/api',
+  API_URL: 'https://development-chose-proposed-winning.trycloudflare.com/api',
   APP_NAME: 'Mi Cine'
 } as const;
