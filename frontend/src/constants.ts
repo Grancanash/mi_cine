8f0538adf713ac06e6c4709e6f51bd094e1688fd
Grancanash <grancanash@gmail.com>
2026-03-13 12:39:48 +0000

    Se ha cambiado la url del cloudflare para mi WPA

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
 //   API_URL: 'http://localhost:8000/api',
 //   API_URL: 'http://217.154.178.102/api',
-  API_URL: 'https://generate-prizes-father-belong.trycloudflare.com/api',
+  API_URL: 'https://oven-relying-addressed-puerto.trycloudflare.com',
   APP_NAME: 'Mi Cine'
 } as const;
