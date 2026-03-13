cbef640a13594249728c646fe454f53004a5c163
Grancanash <grancanash@gmail.com>
2026-03-13 13:54:52 +0000

    Se añade window.location.origin al front para no tener que cambiar más la url

--- backend/mi_cine/settings.py
+++ backend/mi_cine/settings.py
@@ -28,6 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '217.154.178.102',
                  'development-chose-proposed-winning.trycloudflare.com']
 
+#
 
 # Application definition
 
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,9 @@
-export const CONFIG = {
-//   API_URL: 'http://localhost:8000/api',
+// export const CONFIG = {
+// //   API_URL: 'http://localhost:8000/api',
 //   API_URL: 'http://217.154.178.102/api',
-  API_URL: 'https://' + 'development-chose-proposed-winning.trycloudflare.com' + '/api',
+// } as const;
+
+export const CONFIG = {
+  API_URL: `${window.location.origin}/api`,
   APP_NAME: 'Mi Cine'
 } as const;
